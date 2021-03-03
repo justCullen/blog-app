85d40d403d616042a70d554115978431a2240abe
rehack65 <dojojesse@gmail.com>
2021-03-03 14:06:51 -0500

    working in PostCreate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from "./screens/Home/Home"
-// import PostCreate from "./screens/PostCreate/PostCreate"
+import PostCreate from "./screens/PostCreate/PostCreate"
 // import PostEdit from "./screens/PostEdit/PostEdit"
 // import PostDetail from "./screens/PostDetail/PostDetail"
 import { Route, Switch } from 'react-router-dom'
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
-        {/* <Route path="/add-post" component={PostCreate} /> */}
+        <Route path="/add-post" component={PostCreate} />
         {/* <Route exact path="/posts/:id/edit" component={PostEdit} /> */}
         {/* <Route exact path="/posts/:id" component={PostDetail} /> */}
       </Switch>
---
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../components/shared/Layout/Layout'
+import { CreatePost } from '../../services/posts'
+import {useHistory} from 'react-router-dom'
+function PostCreate(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default PostCreate;
