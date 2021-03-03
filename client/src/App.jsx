ace3cd6e0e70e4ddb27f172017b3593d544e9e13
cbscar <cbscar@gmail.com>
2021-03-03 10:57:32 -0600

    file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
 import './App.css';
+import Home from "./screens/Home/Home"
+import PostCreate from "./screens/PostCreate/PostCreate"
+import PostEdit from "./screens/PostEdit/PostEdit"
+import PostDetail from "./screens/PostDetail/PostDetail"
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
@@ -6,7 +10,6 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route  path="/posts" component={Posts} />
         <Route path="/add-post" component={PostCreate} />
         <Route exact path="/posts/:id/edit" component={PostEdit} />
         <Route exact path="/posts/:id" component={PostDetail} />
